100e20c3ce4f99ab77225db29d40563297a4c9ac
micheled <me@thisisdurante.com>
2022-11-30 23:53:53 +0100

    Add base vs project

---
+++ AoC2022/AoC2022.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace AoC2022
+{
+    class AoC2022
+    {
+        private static string[] ReadInput(string name)
+        {
+            return File.ReadAllLines(@".\Data\" + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1: " + Day01.Run(ReadInput("day01.txt")));
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AoC2022/Day01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC2022
+{
+    class Day01
+    {
+        public static (int, int) Run(string[] input)
+        {
+            return (0, 0);
+        }
+    }
+}
