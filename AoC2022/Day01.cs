e8b01209b04b007b5dafc30c16356f6e2434153d
micheled <me@thisisdurante.com>
2022-12-01 13:47:49 +0100

    Add day 1

--- AoC2022/Day01.cs
+++ AoC2022/Day01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AoC2022
@@ -8,7 +9,21 @@ namespace AoC2022
     {
         public static (int, int) Run(string[] input)
         {
-            return (0, 0);
+            var calories = new int[1000];
+            var counter = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length == 0)
+                {
+                    counter++; // next Elf
+                    continue;
+                }
+
+                calories[counter] += int.Parse(input[i]);
+            }
+
+            return (calories.Max(), calories.OrderByDescending(x => x).Take(3).Sum());
         }
     }
 }
